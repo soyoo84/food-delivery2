3bfe2b892230e680d45c40a8d0c72b2f1bf86aea
tommybee <tommybee@naver.com>
2021-02-22 13:51:24 +0900

    add batch files for micro services

---
+++ batch/#2.run_kafka.bat
@@ -0,0 +1,4 @@
+call setenv.bat
+
+kafka-server-start.bat %KAFKA_HOME%\config\server.properties
+
---
+++ batch/setenv.bat
@@ -0,0 +1,10 @@
+SET JAVA_HOME=D:\DEV\SDK\JDK\jdk1.8.0_152
+SET MVN_HOME=C:\DEV\Tools\apache-maven-3.3.3
+SET NODE_HOME=C:\DEV\Tools\node-v14.15.4-win-x64
+SET KAFKA_HOME=D:\DEV\Experiments\CloudLvl2_2\kafka_2.13-2.7.0
+REM SET JASYPT_HOME=C:\DEV\Tools\jasypt-1.9.3
+SET PROJ_HOME=D:\DEV\Experiments\CloudLvl2_3
+SET ANACONDA_HOME=C:\DEV\Tools\Miniconda3
+SET MONGO_HOME=C:\DEV\Tools\Database\mongodb-win32-x86_64-windows-4.4.4
+
+SET PATH=%MONGO_HOME%\BIN;%NODE_HOME%;%NODE_HOME%\node_modules\npm;%NODE_HOME%\node_modules;%KAFKA_HOME%\BIN\WINDOWS;%JAVA_HOME%\BIN;%MVN_HOME%\BIN;%PATH%;
